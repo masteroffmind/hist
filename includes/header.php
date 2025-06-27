46e29d991f2ff1ce5376edc528e72d8388c1a64c
kommers <noisymat1996@gmail.com>
2025-06-28 02:58:33 +0300

    Add files via upload

---
+++ includes/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta name="description" content="Исследование гравюры 1576 года и дипломатической миссии Московского государства к Максимилиану II.">
+    <meta name="keywords" content="гравюра 1576, Иван Грозный, Максимилиан II, Регенсбург, дипломатия, Рюриковичи">
+    <title>Посольство великого князя Московского</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link href="https://fonts.googleapis.com/css2?family=Playfair+Display:wght@400;700&display=swap" rel="stylesheet">
+    <link href="css/style.css" rel="stylesheet">
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/gsap.min.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/gsap@3.12.5/dist/ScrollTrigger.min.js"></script>
+    <script src="js/animations.js"></script>
+    <script src="js/script.js"></script>
+</head>
+<body>
+    <nav class="navbar navbar-expand-lg navbar-dark">
+        <div class="container">
+            <a class="navbar-brand" href="index.php">Гравюра 1576</a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item"><a class="nav-link" href="index.php">Главная</a></li>
+                    <li class="nav-item"><a class="nav-link" href="history.php">История</a></li>
+                    <li class="nav-item"><a class="nav-link" href="engraving.php">Гравюра</a></li>
+                    <li class="nav-item"><a class="nav-link" href="personalities.php">Персоналии</a></li>
+                    <li class="nav-item"><a class="nav-link" href="glossary.php">Словарь</a></li>
+                    <li class="nav-item"><a class="nav-link" href="rovinsky.php">Ровинский</a></li>
+                    <li class="nav-item"><a class="nav-link" href="contacts.php">Контакты</a></li>
+                </ul>
+            </div>
+        </div>
+    </nav>
